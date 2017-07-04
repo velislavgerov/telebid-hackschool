1afa2ee30c9901426eb6e9937e0af178b5b692ee
thunderstruck47 <velislav.gerov.12@aberdeen.ac.uk>
2017-07-04 17:14:13 +0300

    added a Ping class to work with our ping object

--- AMTP/main.py
+++ AMTP/main.py
@@ -8,24 +8,73 @@ from geventhttpclient.url import URL
 import os
 import time
 
+REQUEST_TIMEOUT = 100
+REQUESTS_COUNT = 1
+
 class Ping(object):
     """
     A class to work with our ping object as specified in TBMON
     """
     def __init__(self, data):
         """
-        Accepts a single TBMON ping object
+        Accepts a single TBMON ping object and creates our http object
         """
         # TODO: validate data
         self.data = data
+        
+        try:
+            self.request_timeout = float(data['request_timeout'])
+        except KeyError:
+            self.request_timeout = REQUEST_TIMEOUT
+        try:
+            self.requests_count = float(data['request_timeout'])
+        except KeyError:
+            self.requests_count = REQUESTS_COUNT
+        
+        self.url = URL(data['url'])
+        self.http = HTTPClient(self.url.host, connection_timeout=self.request_timeout)
 
+   
+    def get_response(self):
+        """
+        Makes a request and returns a geventhttpclient response object
+        """
+        return self.http.get(self.url.request_uri)
+
+    def _test_expected_response_codes(self, response):
+        """
+        Returns True if one of the specified response codes is in the response
+        
+        :response - geventhttpclient response object
+        """
+        if response.status_code in [int(x) for x in self.data['expected_response_codes']]:
+            return True
+        else:
+            return False
+    
+    def _test_expected_header(self, response):
+        """
+        Returns True if one of the expected headers is in the response headers
+        
+        :response - geventhttpclient response object
+        """
+        for field, value in [x.split(':',1) for x in self.data['expected_headers']]:
+            if value == response._headers_index.get_all(field)[0]:
+                return True
+        return False
+
+    def _test_expected_body(self, response_body):
+        """
+        Returns True if the string specified is to be found in the body of the response
+        
+        :response_body - byte string containing the body of the HTTP response
+        """
+        return True if self.data['expected_response_body'][0].encode() in response_body else False
 
 class HTTPPinger(object):
     """
     Our main application object. Responsible for managing and conducting the pings
     """
-    REQUEST_TIMEOUT = 100
-    REQUESTS_COUNT = 1
 
     def __init__(self, data):
         """
@@ -41,28 +90,19 @@ class HTTPPinger(object):
 
             for p in pings:
                 name = pings[p]['name']
-                url = URL(pings[p]['url'])
-                erc = pings[p]['expected_response_codes']
-                ehd = pings[p]['expected_headers']
-                erb = pings[p]['expected_response_body'][0]
-                try:
-                    timeout = float(pings[p]['request_timeout'])
-                except KeyError:
-                    timeout = self.REQUEST_TIMEOUT
-                try:
-                    requests_count = float(pings[p]['request_timeout'])
-                except KeyError:
-                    requests_count = self.REQUESTS_COUNT
+                print("Ping started ({})".format(name))
 
-                # making a request
-                http = HTTPClient(url.host, connection_timeout=timeout)
-                res = http.get(url.request_uri)
-                
+                # create our ping object
+                ping = Ping(pings[p])
+
+                # do the ping
+                res = ping.get_response()
+
                 # do the tests
-                is_erc = self._test_expected_response_codes(erc, res)
-                is_ehd = self._test_expected_header(ehd, res)
+                is_erc = ping._test_expected_response_codes(res)
+                is_ehd = ping._test_expected_header(res)
                 body = res.read() # bytes
-                is_erb = self._test_expected_body(erb, body)
+                is_erb = ping._test_expected_body(body)
                 
                 if __debug__:
                     print("Testing expected status code - {}".format(
